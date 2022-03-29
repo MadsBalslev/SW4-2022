322d4ba31ab1381870eddb844311b1e1b299c732
Casper Ståhl <cstahl20@student.aau.dk>
2022-03-29 10:17:29 +0200

    Initialize maven project

---
+++ fannie-compiler/src/main/java/cs22sw405/App.java
@@ -0,0 +1,13 @@
+package cs22sw405;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
