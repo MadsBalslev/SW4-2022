5a2712501e29a130df1e4224aa71978edae7b8f2
Casper Ståhl <cstahl20@student.aau.dk>
2022-03-29 13:12:33 +0200

    Add testing of CodeGeneratorVisitor
    Made template of pasta in MarkDown
    
    Update Lasagne.fannie
    
    maven
    
    deleted files
    
    Create formal_semantic_specification.md
    
    Update formal_semantic_specification.md
    
    Update settings.json
    
    Update formal_semantic_specification.md
    
    ASTNodes and visitorbuilder
    
    created small subset of ASTnodes and tried creating a VisitorBuilder
    
    Interpretervisitor and pom.xml update
    
    Tjek imorgen
    
    Ved ik hvad der lige skete i mit hoved men jeg lavede et eller andet
    
    Interpreter visitor
    
    git gud
    
    Pulled down CodeGenerator interpreter
    
    Create InterpreterVisitor.java
    
    surely this is fine
    
    end of day merge
    
    we enjoy
    
    nicolai changes
    
    read/write works lol
    
    Working prototype for CodeGenerateVisitor
    
    Change path to relative
    
    yep
    
    Changed markdownFormat to use StringBuilder
    
    Rebase with HEAD
    
    Add tests

---
+++ fannie-compiler/src/main/java/App.java
@@ -0,0 +1,22 @@
+import java.io.InputStream;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        InputStream fileContent = ReadFile.read(args[0]);
+        CharStream input = CharStreams.fromStream(fileContent);
+        fannieParserLexer lexer = new fannieParserLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        fannieParserParser parser = new fannieParserParser(tokens);
+
+        ParseTree tree = parser.fannie();
+
+        CodeGeneratorVisitor cgv = new CodeGeneratorVisitor();
+        cgv.visit(tree);
+
+        // System.out.println(cgv.markdownFormat);
+
+        WriteToFile.write("fannie.md", cgv.markdownFormat);
+    }
+}
