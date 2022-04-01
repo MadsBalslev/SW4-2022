426609bf95b760fe4298e0c8e889b1d83d149c42
pberte20 <71381584+pberte20@users.noreply.github.com>
2022-04-01 09:51:43 +0200

    Interpreter visitor

--- fannie-compiler/src/main/java/InterpreterVisitor.java
+++ fannie-compiler/src/main/java/InterpreterVisitor.java
@@ -1,19 +1,42 @@
-
-
-public class InterpreterVisitor extends fannieParserBaseVisitor<Token>{
-
-    @Override public Token visitFannie(fannieParserParser.FannieContext context)
-    {
-        return visitChildren(context);
-    }
-    @Override public Token visitMainRecipe(fannieParserParser.MainRecipeContext context)
-    {
-        return visitChildren(context);
-    }
-    @Override public Token visitSubRecipe(fannieParserParser.SubRecipeContext context) 
+public class InterpreterVisitor extends fannieParserBaseVisitor<Void> {
+    public Void visitFannie(fannieParserParser.FannieContext context) 
     { 
-        return visitChildren(context); 
+        visitChildren(context); 
+        return null;
+    }   
+    public Void visitMainRecipe(fannieParserParser.MainRecipeContext context) 
+    { 
+        visitChildren(context); 
+        return null;
     }
-    
-   
+    public Void visitSubRecipe(fannieParserParser.SubRecipeContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    public Void visitRecipeIdentifier(fannieParserParser.RecipeIdentifierContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    public Void visitRecipeBody(fannieParserParser.RecipeBodyContext context) 
+    { 
+        visitChildren(context); 
+        return null;
+    }
+    public Void visitIngredientsList(fannieParserParser.IngredientsListContext context) 
+    { 
+        visitChildren(context); 
+        return null;
+    }
+    public Void visitToolsList(fannieParserParser.ToolsListContext context) 
+    {
+        visitChildren(context);
+        return null;
+    }
+    public Void visitStepsList(fannieParserParser.StepsListContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
 }
