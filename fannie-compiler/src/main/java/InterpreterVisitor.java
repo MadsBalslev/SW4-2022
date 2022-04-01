834592ecab5d4ccaf1dc0ace87100ec070e5e747
pberte20 <71381584+pberte20@users.noreply.github.com>
2022-04-01 15:12:45 +0200

    Updated interpretervisitor

--- fannie-compiler/src/main/java/InterpreterVisitor.java
+++ fannie-compiler/src/main/java/InterpreterVisitor.java
@@ -1,4 +1,7 @@
+import java.util.List;
 public class InterpreterVisitor extends fannieParserBaseVisitor<Void> {
+    List<String> toolsList;
+    List<String> recipesList;
     @Override public Void visitFannie(fannieParserParser.FannieContext context) 
     { 
         visitChildren(context); 
@@ -6,35 +9,157 @@ public class InterpreterVisitor extends fannieParserBaseVisitor<Void> {
     }   
     @Override public Void visitMainRecipe(fannieParserParser.MainRecipeContext context) 
     { 
+        recipesList.add(context.recipeIdentifier().getText());
         visitChildren(context); 
         return null;
     }
     @Override public Void visitSubRecipe(fannieParserParser.SubRecipeContext context) 
     { 
+        recipesList.add(context.recipeIdentifier().getText());
         visitChildren(context);
         return null;
     }
-    @ Override public Void visitRecipeIdentifier(fannieParserParser.RecipeIdentifierContext context) 
+    @Override public Void visitRecipeIdentifier(fannieParserParser.RecipeIdentifierContext context) 
     { 
         visitChildren(context);
         return null;
     }
-    public Void visitRecipeBody(fannieParserParser.RecipeBodyContext context) 
+    @Override public Void visitRecipeBody(fannieParserParser.RecipeBodyContext context) 
     { 
         visitChildren(context); 
         return null;
     }
-    public Void visitIngredientsList(fannieParserParser.IngredientsListContext context) 
+    @Override public Void visitIngredientsList(fannieParserParser.IngredientsListContext context) 
     { 
         visitChildren(context); 
         return null;
     }
-    public Void visitToolsList(fannieParserParser.ToolsListContext context) 
+    @Override public Void visitToolsList(fannieParserParser.ToolsListContext context) 
     {
         visitChildren(context);
         return null;
     }
-    public Void visitStepsList(fannieParserParser.StepsListContext context) 
+    @Override public Void visitStepsList(fannieParserParser.StepsListContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitIngredientDeclaration(fannieParserParser.IngredientDeclarationContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitToolDeclaration(fannieParserParser.ToolDeclarationContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitStepDeclaration(fannieParserParser.StepDeclarationContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitIngredientIdentifier(fannieParserParser.IngredientIdentifierContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitToolIdentifier(fannieParserParser.ToolIdentifierContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitDeterministicIngredientDeclaration(fannieParserParser.DeterministicIngredientDeclarationContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitNondeterministicIngredientDeclaration(fannieParserParser.NondeterministicIngredientDeclarationContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitRecipeIngredientDeclaration(fannieParserParser.RecipeIngredientDeclarationContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitIngredientTypeDeclaration(fannieParserParser.IngredientTypeDeclarationContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitAmountDeclaration(fannieParserParser.AmountDeclarationContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitProcIdentifier(fannieParserParser.ProcIdentifierContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitToolActionIdentifier(fannieParserParser.ToolActionIdentifierContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitToolTypeIdentifier(fannieParserParser.ToolTypeIdentifierContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitIngredientTypeIdentifier(fannieParserParser.IngredientTypeIdentifierContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitToolActionDeclarationsList(fannieParserParser.ToolActionDeclarationsListContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitToolActionDeclaration(fannieParserParser.ToolActionDeclarationContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitServeStepDeclaration(fannieParserParser.ServeStepDeclarationContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitDoStepDeclaration(fannieParserParser.DoStepDeclarationContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitContinousDoStepStartDeclaration(fannieParserParser.ContinousDoStepStartDeclarationContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitContinousDoStepStopDeclaration(fannieParserParser.ContinousDoStepStopDeclarationContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitStepIn(fannieParserParser.StepInContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitStepOut(fannieParserParser.StepOutContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitContentIn(fannieParserParser.ContentInContext context) 
+    { 
+        visitChildren(context);
+        return null;
+    }
+    @Override public Void visitIngredientCollection(fannieParserParser.IngredientCollectionContext context) 
     { 
         visitChildren(context);
         return null;
--- fannie-compiler/src/main/java/cs22sw405/App.java
+++ fannie-compiler/src/main/java/cs22sw405/App.java
@@ -1,5 +1,4 @@
 package cs22sw405;
-
 /**
  * Hello world!
  *
