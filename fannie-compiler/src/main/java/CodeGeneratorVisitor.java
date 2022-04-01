79bcdf25d25fb692cf9ee0019bd83916a4534d3f
uglenDX <smpandersen@gmail.com>
2022-04-01 13:31:46 +0200

    git gud

---
+++ fannie-compiler/src/main/java/CodeGeneratorVisitor.java
@@ -0,0 +1,17 @@
+public class CodeGeneratorVisitor extends fannieParserBaseVisitor<Void> {
+
+    public Void RecipeNode(ProgramContext ctx)
+    {
+      return null;
+    }
+
+    @Override public Void visitRecipeIdentifier(fannieParserParser.RecipeIdentifierContext ctx) 
+    {
+      return null;
+    }
+
+
+
+
+    
+}
