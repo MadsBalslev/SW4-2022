42c474832a0f56323cafe00094185f27f9c3157d
Mads Balslev <madspbalslev@gmail.com>
2022-05-19 14:17:54 +0200

    PDF are now generating (#215)

--- fannie-compiler/src/main/java/App.java
+++ fannie-compiler/src/main/java/App.java
@@ -11,17 +11,18 @@ public class App {
         CharStream input = CharStreams.fromStream(fileContent);
         fannieParserLexer lexer = new fannieParserLexer(input);
 
-        // ERROR LISTENER FROM https://stackoverflow.com/questions/18132078/handling-errors-in-antlr4
+        // ERROR LISTENER FROM
+        // https://stackoverflow.com/questions/18132078/handling-errors-in-antlr4
         lexer.removeErrorListeners();
         lexer.addErrorListener(ThrowingErrorListener.INSTANCE);
 
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         fannieParserParser parser = new fannieParserParser(tokens);
 
-        // ERROR LISTENER FROM https://stackoverflow.com/questions/18132078/handling-errors-in-antlr4
+        // ERROR LISTENER FROM
+        // https://stackoverflow.com/questions/18132078/handling-errors-in-antlr4
         parser.removeErrorListeners();
         parser.addErrorListener(ThrowingErrorListener.INSTANCE);
-
 
         ParseTree tree = parser.fannie();
         try {
@@ -29,19 +30,19 @@ public class App {
         } catch (CompilerException e) {
             System.out.println("[ERROR] " + e.getMessage());
         }
-        
 
         CodeGeneratorVisitor cgv = new CodeGeneratorVisitor();
         cgv.visit(tree);
-        
+
         String HTML = FileConvert.MDToHTML(cgv.markdownFormat);
+        String basePath = "../output/";
+        WriteToFile.write(basePath + "output.html", HTML);
+        WriteToFile.write(basePath + "output.md", cgv.markdownFormat.toString());
         try {
-            FileConvert.HTMLToPDF(HTML);
+            FileConvert.HTMLToPDF(basePath, HTML);
         } catch (Exception e) {
             System.out.println("Could not generate PDF");
         }
         // String basePath = "./output/";
-        WriteToFile.write("../output/output.html", HTML);
-        WriteToFile.write("../output/output.md", cgv.markdownFormat.toString());
     }
 }
